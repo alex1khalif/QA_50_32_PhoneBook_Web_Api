534aeb68e4a10eaa72c2199295ffd370db1f0b99
alexkhalifa <alexkhalifamusic@gmail.com>
2026-01-20 20:09:33 +0200

    Login positive test

--- src/test/java/ui_tests/LoginTests.java
+++ src/test/java/ui_tests/LoginTests.java
@@ -1,8 +1,10 @@
 package ui_tests;
 
+import dto.User;
 import manager.AppManager;
 import org.testng.annotations.Test;
 import pages.HomePage;
+import pages.LoginPage;
 
 public class LoginTests extends AppManager {
     @Test
@@ -12,5 +14,23 @@ public class LoginTests extends AppManager {
 
         HomePage homePage = new HomePage(getDriver());
         homePage.clickBtnLogin();
+
+        LoginPage loginPage = new LoginPage(getDriver());
+        loginPage.typeLoginRegistrationForm("alex1khalif9999@gmail.com", "Qwerty474849!");
+        loginPage.clickBtnLoginForm();
+
+    }
+
+    @Test
+    public void loginPositiveTestWithUser()
+    {
+        User user = new User("alex1khalif9999@gmail.com", "Qwerty474849!");
+        HomePage homePage = new HomePage(getDriver());
+        homePage.clickBtnLogin();
+
+        LoginPage loginPage = new LoginPage(getDriver());
+        loginPage.typeLoginRegistrationFormWithUser(user);
+        loginPage.clickBtnLoginForm();
+
     }
 }
