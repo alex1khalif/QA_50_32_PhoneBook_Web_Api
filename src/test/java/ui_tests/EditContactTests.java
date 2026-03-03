424b29fff55f0e6cbe596fa323647c76ea9988a1
alexkhalifa <alexkhalifamusic@gmail.com>
2026-03-03 18:21:42 +0200

    EditContact Test With CardOfContact

--- src/test/java/ui_tests/EditContactTests.java
+++ src/test/java/ui_tests/EditContactTests.java
@@ -5,6 +5,7 @@ import manager.AppManager;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 import pages.BasePage;
 import pages.ContactPage;
 import pages.HomePage;
@@ -15,6 +16,7 @@ import static utils.HeaderMenuItem.LOGIN;
 import static utils.PropertiesReader.getProperty;
 
 public class EditContactTests extends AppManager {
+    SoftAssert softAssert = new SoftAssert();
     HomePage homePage;
     LoginPage loginPage;
     ContactPage contactPage;
@@ -38,5 +40,17 @@ public class EditContactTests extends AppManager {
         Assert.assertTrue(contactPage.isContactPresent(contact));
 
 
+    }
+
+    @Test
+    public void editFirstContactPositiveTest_WithCardOfContact(){
+        Contact contact = ContactFactory.positiveContact();
+        contactPage.typeEditForm(contact);
+        contactPage.pause(3);
+        String text = contactPage.getTextInContact();
+        softAssert.assertTrue(text.contains(contact.getName()), "validate Name in DetailCard");
+        softAssert.assertTrue(text.contains(contact.getEmail()), "validate Email in DetailCard");
+        softAssert.assertTrue(text.contains(contact.getPhone()), "validate Phone in DetailCard");
+        softAssert.assertAll();
     }
 }
